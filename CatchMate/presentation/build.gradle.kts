d85ed1df27222658e34739beee62ffb182f521af
lge6716@gmail.com <lge6716@gmail.com>
2024-06-20 00:23:22 +0900

    chore: presentation 모듈에서 nested scroll view 사용을 위한 legacy 의존성 추가

--- CatchMate/presentation/build.gradle.kts
+++ CatchMate/presentation/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
+    implementation(libs.legacy.support.v4)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
