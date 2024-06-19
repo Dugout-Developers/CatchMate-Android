b07c9f798aeb4c116df864fbed1650414ee5bad5
lge6716@gmail.com <lge6716@gmail.com>
2024-06-20 00:31:51 +0900

    feat: presentation 모듈에 Main Activity 생성

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.catchmate.presentation.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.catchmate.presentation.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
