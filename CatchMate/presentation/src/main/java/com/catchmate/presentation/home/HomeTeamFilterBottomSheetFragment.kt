e948d38fe32ce549eb87f3f38c5fee14b291d86a
lge6716@gmail.com <lge6716@gmail.com>
2024-06-21 23:46:27 +0900

    design: 구단 필터 Bottom sheet ui 구현
    - 구단 로고 파일 오류로 인해 추후에 변경 필요함

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/home/HomeTeamFilterBottomSheetFragment.kt
@@ -0,0 +1,19 @@
+package com.catchmate.presentation.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.catchmate.presentation.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class HomeTeamFilterBottomSheetFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home_team_filter_bottom_sheet, container, false)
+    }
+    // 체크박스마다 setOnCheckedChangeListener 달아서 이미지뷰 리소스를 변경하기
+}
