9846d54e89a2db7eff9566e0965be3ed2fc803f1
lge6716@gmail.com <lge6716@gmail.com>
2024-06-27 15:07:10 +0900

    design: Signup Complete Fragment UI 구현

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/onboarding/SignupCompleteFragment.kt
@@ -0,0 +1,31 @@
+package com.catchmate.presentation.view.onboarding
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.catchmate.presentation.R
+import com.catchmate.presentation.databinding.FragmentSignupCompleteBinding
+
+class SignupCompleteFragment : Fragment() {
+    private var _binding : FragmentSignupCompleteBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignupCompleteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.layoutSignupCompleteButton.btnFooterOne.setText(R.string.complete)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
