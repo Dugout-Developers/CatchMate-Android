60f7eb67a072cad30fe2d5ebe0b127afecc5a87c
lge6716@gmail.com <lge6716@gmail.com>
2024-07-10 00:43:15 +0900

    ui: Read Post Fragment UI 구현
    - 케밥, 오른쪽 화살표, 찜 아이콘 리소스 등록
    - 찜 버튼이 있는 footer layout 생성
    - 케밥 메뉴가 있는 header layout 생성

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/post/ReadPostFragment.kt
@@ -0,0 +1,19 @@
+package com.catchmate.presentation.view.post
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.catchmate.presentation.R
+
+class ReadPostFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_read_post, container, false)
+    }
+}
