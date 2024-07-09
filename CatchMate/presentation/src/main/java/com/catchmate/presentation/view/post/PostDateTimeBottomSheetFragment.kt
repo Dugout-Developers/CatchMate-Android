fac30f21a2e23aa9af6db15eb28250588395e5a9
lge6716@gmail.com <lge6716@gmail.com>
2024-07-09 18:01:52 +0900

    ui: 날짜 및 시간 선택 Bottom Sheet UI 구현

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/post/PostDateTimeBottomSheetFragment.kt
@@ -0,0 +1,19 @@
+package com.catchmate.presentation.view.post
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.catchmate.presentation.R
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class PostDateTimeBottomSheetFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post_date_time_bottom_sheet, container, false)
+    }
+}
