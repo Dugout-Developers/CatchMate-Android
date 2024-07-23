324f5610a91e28dddcea9f2d54684306717627ce
lge6716@gmail.com <lge6716@gmail.com>
2024-07-23 23:30:02 +0900

    chore: root 수준 build.gradle 수정 - buildscript 구문 삭제 - hilt 플러그인 추가

--- CatchMate/build.gradle.kts
+++ CatchMate/build.gradle.kts
@@ -1,13 +1,8 @@
-buildscript {
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
-    }
-}
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.com.android.library) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.hilt.android) apply false
 }
 true // Needed to make the Suppress annotation work for
