15ff8635ac6cc15379d39cec7a9a0efa3848c2ac
lge6716@gmail.com <lge6716@gmail.com>
2024-07-23 23:30:51 +0900

    chore: domain 모듈 build.gradle 수정

--- CatchMate/domain/build.gradle.kts
+++ CatchMate/domain/build.gradle.kts
@@ -1,9 +1,7 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id("catchmate.android.library")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+android {
+    namespace = "com.catchmate.domain"
 }
