3122a13e4f8678d66c79a9167bfbe8447aa66001
lge6716@gmail.com <lge6716@gmail.com>
2024-07-24 23:45:26 +0900

    feat: Application 클래스에서 Naver Login SDK 초기화

--- CatchMate/app/src/main/java/com/catchmate/android/Application.kt
+++ CatchMate/app/src/main/java/com/catchmate/android/Application.kt
@@ -2,6 +2,7 @@ package com.catchmate.android
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -11,5 +12,13 @@ class Application : Application() {
 
         // kakao sdk 초기화
         KakaoSdk.init(this, "${BuildConfig.KAKAO_NATIVE_APP_KEY}")
+
+        // naver sdk 초기화
+        NaverIdLoginSDK.initialize(
+            this,
+            "${BuildConfig.NAVER_CLIENT_ID}",
+            "${BuildConfig.NAVER_CLIENT_SECRET}",
+            "CatchMate",
+        )
     }
 }
