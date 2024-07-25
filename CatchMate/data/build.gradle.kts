4cd663b6bf37ccce9a399282fafc427deea59fa5
lge6716@gmail.com <lge6716@gmail.com>
2024-07-25 17:51:25 +0900

    chore: Google auth 구현을 위한 Gradle 설정

--- CatchMate/data/build.gradle.kts
+++ CatchMate/data/build.gradle.kts
@@ -1,11 +1,27 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     id("catchmate.android.library")
     id("catchmate.android.hilt")
     id("catchmate.kotlin.hilt")
 }
 
+val properties = Properties()
+properties.load(FileInputStream(rootProject.file("local.properties")))
+
 android {
     namespace = "com.catchmate.data"
+
+    val googleWebClientId = properties["google_web_client_id"] as? String ?: ""
+
+    defaultConfig {
+        buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", googleWebClientId)
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -13,4 +29,7 @@ dependencies {
 
     implementation(libs.kakao.user)
     implementation(libs.naver.user)
+    implementation(libs.androidx.credentials)
+    implementation(libs.androidx.credentials.play.services.auth)
+    implementation(libs.googleid)
 }
