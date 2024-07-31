65408eeac78449b21592ddbb9d903b30c0b07ed8
nueijeel <lge6716@gmail.com>
2024-07-31 14:34:22 +0900

    chore: 서버 도메인 BuildConfig 객체로 저장

--- CatchMate/data/build.gradle.kts
+++ CatchMate/data/build.gradle.kts
@@ -14,9 +14,11 @@ android {
     namespace = "com.catchmate.data"
 
     val googleWebClientId = properties["google_web_client_id"] as? String ?: ""
+    val serverDomain = properties["server_domain"] as? String ?: ""
 
     defaultConfig {
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", googleWebClientId)
+        buildConfigField("String", "SERVER_DOMAIN", serverDomain)
     }
 
     buildFeatures {
