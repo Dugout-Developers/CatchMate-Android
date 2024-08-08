e55eadc24292444bdfbc038ac92bd71dee9f194f
nueijeel <lge6716@gmail.com>
2024-08-08 15:38:47 +0900

    design: Filter View 커스텀

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFilterView.kt
@@ -0,0 +1,56 @@
+package com.catchmate.presentation.view.home
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.content.res.TypedArray
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import com.catchmate.presentation.R
+import com.catchmate.presentation.databinding.ViewHomeFilterBinding
+
+class HomeFilterView : ConstraintLayout {
+    var isFilterSet = false
+    lateinit var layoutFilter: ConstraintLayout
+    lateinit var tvFilterName: TextView
+    lateinit var ivFilterDropdown: ImageView
+    constructor(context: Context) : super(context) {
+        initView()
+    }
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        initView()
+        getAttrs(attrs)
+    }
+
+    private val binding: ViewHomeFilterBinding by lazy {
+        ViewHomeFilterBinding.bind(
+            LayoutInflater.from(context).inflate(R.layout.view_home_filter, this, false)
+        )
+    }
+
+    private fun initView() {
+        addView(binding.root)
+        layoutFilter = binding.layoutFilter
+        tvFilterName = binding.tvFilterName
+        ivFilterDropdown = binding.ivFilterDropdown
+    }
+
+    private fun getAttrs(attrs: AttributeSet) {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.HomeFilterView)
+        setTypedArray(typedArray)
+    }
+
+    private fun setTypedArray(typedArray: TypedArray) {
+        val homeFilterLayoutColor = typedArray.getColor(R.styleable.HomeFilterView_homeFilterLayoutColor, -1)
+        val homeFilterNameText = typedArray.getText(R.styleable.HomeFilterView_homeFilterNameText)
+        val homeFilterImage = typedArray.getResourceId(R.styleable.HomeFilterView_homeFilterImage, -1)
+
+        ViewCompat.setBackgroundTintList(layoutFilter, ColorStateList.valueOf(homeFilterLayoutColor))
+        tvFilterName.text = homeFilterNameText
+        ivFilterDropdown.setImageResource(homeFilterImage)
+    }
+}
