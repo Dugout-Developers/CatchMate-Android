e54bd15bf2b850cf766bd1ed4cbeb7439fe0c043
nueijeel <lge6716@gmail.com>
2024-08-08 15:40:16 +0900

    feature: Home Fragment 알림 버튼 및 필터 뷰 클릭 리스너 등록

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.catchmate.presentation.view.home
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.catchmate.presentation.R
 import com.catchmate.presentation.databinding.FragmentHomeBinding
 
@@ -26,8 +30,11 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        initHeader()
         initDateFilter()
         initTeamFilter()
+        initHeadCountFilter()
     }
 
     override fun onDestroyView() {
@@ -35,14 +42,32 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
+    private fun initHeader() {
+        binding.layoutHeaderHome.apply {
+            imgbtnHeaderHomeNotification.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_notificationFragment)
+            }
         }
     }
 
     private fun initDateFilter() {
+        binding.hfvHomeDateFilter.setOnClickListener {
+            val dateFilterBottomSheet = HomeDateFilterBottomSheetFragment()
+            dateFilterBottomSheet.show(requireActivity().supportFragmentManager, dateFilterBottomSheet.tag)
         }
     }
 
     private fun initTeamFilter() {
+        binding.hfvHomeTeamFilter.setOnClickListener {
+            val teamFilterBottomSheet = HomeTeamFilterBottomSheetFragment()
+            teamFilterBottomSheet.show(requireActivity().supportFragmentManager, teamFilterBottomSheet.tag)
+        }
+    }
+
+    private fun initHeadCountFilter() {
+        binding.hfvHomeMemberCountFilter.setOnClickListener {
+            val headCountFilterBottomSheet = HomeHeadCountFilterBottomSheetFragment()
+            headCountFilterBottomSheet.show(requireActivity().supportFragmentManager, headCountFilterBottomSheet.tag)
         }
     }
 }
