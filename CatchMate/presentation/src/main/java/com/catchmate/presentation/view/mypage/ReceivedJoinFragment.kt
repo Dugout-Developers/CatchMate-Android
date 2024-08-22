8b5a5b6c892d48c44c7cebbe196a053aa2f0c9db
nueijeel <lge6716@gmail.com>
2024-08-22 16:23:37 +0900

    ui: Received Join Fragment ui 수정

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/ReceivedJoinFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/ReceivedJoinFragment.kt
@@ -6,14 +6,38 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.catchmate.presentation.R
+import com.catchmate.presentation.databinding.FragmentReceivedJoinBinding
 
 class ReceivedJoinFragment : Fragment() {
+    private var _binding: FragmentReceivedJoinBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_received_join, container, false)
+        _binding = FragmentReceivedJoinBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initHeader()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initHeader() {
+        binding.layoutHeaderReceivedJoin.apply {
+            tvHeaderTextTitle.setText(R.string.mypage_received_join)
+            tvHeaderTextUnreadMessageCountBadge.visibility = View.VISIBLE // message 확인 여부에 따라 유동적으로 설정하기
+        }
     }
 }
