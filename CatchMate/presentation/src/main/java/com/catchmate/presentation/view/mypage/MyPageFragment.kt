46b2b637b7b097e59accff363e56f41082d05bbc
nueijeel <lge6716@gmail.com>
2024-08-22 16:08:34 +0900

    ui: My Page Fragment ui 수정

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/MyPageFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/MyPageFragment.kt
@@ -5,15 +5,39 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.catchmate.presentation.R
+import com.catchmate.presentation.databinding.FragmentMyPageBinding
 
 class MyPageFragment : Fragment() {
+    private var _binding: FragmentMyPageBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_page, container, false)
+        _binding = FragmentMyPageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initHeader()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initHeader() {
+        binding.layoutHeaderMyPage.apply {
+            tvSettingHeaderTitle.setText(R.string.mypage_title)
+        }
     }
 }
