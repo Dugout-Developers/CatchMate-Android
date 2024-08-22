fe6817b5035e61f8b509ac9e47cdbb0745cd5cf1
nueijeel <lge6716@gmail.com>
2024-08-22 16:11:05 +0900

    ui: Edit Profile Fragment ui 수정

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/EditProfileFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/EditProfileFragment.kt
@@ -5,15 +5,42 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.catchmate.presentation.R
+import com.catchmate.presentation.databinding.FragmentEditProfileBinding
 
 class EditProfileFragment : Fragment() {
+    private var _binding: FragmentEditProfileBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+        _binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        initHeader()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initHeader() {
+        binding.layoutHeaderEditProfile.apply {
+            imgbtnHeaderTextBack.setOnClickListener {
+                findNavController().popBackStack()
+            }
+            tvHeaderTextTitle.text = getString(R.string.edit_profile_title)
+        }
     }
 }
