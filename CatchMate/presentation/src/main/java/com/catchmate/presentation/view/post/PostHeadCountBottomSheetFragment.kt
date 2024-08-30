9b4a4e28306b65d0bd9a7a29b095b34e502beff7
nueijeel <lge6716@gmail.com>
2024-08-30 22:05:21 +0900

    feature: 인원 수 선택 Bottom Sheet 구현

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/post/PostHeadCountBottomSheetFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/post/PostHeadCountBottomSheetFragment.kt
@@ -6,15 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import com.catchmate.presentation.R
 import com.catchmate.presentation.databinding.FragmentPostHeadCountBottomSheetBinding
+import com.catchmate.presentation.interaction.OnPeopleCountSelectedListener
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class PostHeadCountBottomSheetFragment : BottomSheetDialogFragment() {
     private var _binding: FragmentPostHeadCountBottomSheetBinding? = null
     val binding get() = _binding!!
 
+    private var peopleCountSelectedListener: OnPeopleCountSelectedListener? = null
+
     private val headCountArray =
         arrayOf(
-            "1명",
             "2명",
             "3명",
             "4명",
@@ -47,10 +49,18 @@ class PostHeadCountBottomSheetFragment : BottomSheetDialogFragment() {
         _binding = null
     }
 
+    fun setOnPeopleCountSelectedListener(listener: OnPeopleCountSelectedListener) {
+        peopleCountSelectedListener = listener
+    }
+
     private fun initFooterButton() {
         binding.layoutPostHeadCountFooter.btnFooterOne.run {
             isEnabled = true
-            setText(R.string.application)
+            setText(R.string.complete)
+            setOnClickListener {
+                peopleCountSelectedListener?.onPeopleCountSelected(binding.npPostHeadCount.value + 2)
+                dismiss()
+            }
         }
     }
 
