6b31be7ef4916a69a26e2fe20c1249332bf3e3c9
nueijeel <lge6716@gmail.com>
2024-09-03 01:01:23 +0900

    refactor: ktlint에 맞게 수정

--- CatchMate/domain/src/main/java/com/catchmate/domain/usecase/SignUpUseCase.kt
+++ CatchMate/domain/src/main/java/com/catchmate/domain/usecase/SignUpUseCase.kt
@@ -5,15 +5,16 @@ import com.catchmate.domain.model.UserAdditionalInfoRequest
 import com.catchmate.domain.model.UserResponse
 import com.catchmate.domain.repository.SignUpRepository
 import javax.inject.Inject
-
 class SignUpUseCase
     @Inject
     constructor(
         private val signUpRepository: SignUpRepository,
     ) {
-        suspend fun getNicknameAvailability(nickName: String): CheckNicknameResponse? =
+        suspend fun getNicknameAvailability(nickName: String): CheckNicknameResponse? {
             signUpRepository.getNicknameAvailability(nickName)
+        }
 
         suspend fun postUserAdditionalInfo(userAdditionalInfoRequest: UserAdditionalInfoRequest): UserResponse? =
             signUpRepository.postUserAdditionalInfo(userAdditionalInfoRequest)
     }
+
