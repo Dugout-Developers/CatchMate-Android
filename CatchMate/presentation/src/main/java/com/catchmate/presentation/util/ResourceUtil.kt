94dd87662134af34634edb3abb7990b95ff116fa
nueijeel <lge6716@gmail.com>
2024-09-06 00:17:39 +0900

    feature: 리소스 처리 로직 ResourceUtil 생성

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/util/ResourceUtil.kt
@@ -0,0 +1,50 @@
+package com.catchmate.presentation.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.catchmate.presentation.R
+
+object ResourceUtil {
+    fun convertTeamLogo(teamName: String): Int {
+        return when (teamName) {
+            "다이노스" -> R.drawable.vec_all_nc_dinos_logo
+            "라이온즈" -> R.drawable.vec_all_samsung_lions_logo
+            "랜더스" -> R.drawable.vec_all_ssg_landers_logo
+            "베어스" -> R.drawable.vec_all_doosan_bears_logo
+            "위즈" -> R.drawable.vec_all_kt_wiz_logo
+            "이글스" -> R.drawable.vec_all_hanwha_eagles_logo
+            "자이언츠" -> R.drawable.vec_all_lotte_giants_logo
+            "타이거즈" -> R.drawable.vec_all_kia_tigers_logo
+            "트윈스" -> R.drawable.vec_all_lg_twins_logo
+            else -> R.drawable.vec_all_kiwoom_heroes_logo
+        }
+    }
+
+    fun convertTeamColor(context: Context, teamName: String, isCheerTeam: Boolean): Int {
+        return if (isCheerTeam) {
+            when (teamName) {
+                "다이노스" -> ContextCompat.getColor(context, R.color.nc_dinos)
+                "라이온즈" -> ContextCompat.getColor(context, R.color.samsung_lions)
+                "랜더스" -> ContextCompat.getColor(context, R.color.ssg_landers)
+                "베어스" -> ContextCompat.getColor(context, R.color.doosan_bears)
+                "위즈" -> ContextCompat.getColor(context, R.color.kt_wiz)
+                "이글스" -> ContextCompat.getColor(context, R.color.hanwha_eagles)
+                "자이언츠" -> ContextCompat.getColor(context, R.color.lotte_giants)
+                "타이거즈" -> ContextCompat.getColor(context, R.color.kia_tigers)
+                "트윈스" -> ContextCompat.getColor(context, R.color.lg_twins)
+                else -> ContextCompat.getColor(context, R.color.kiwoom_heroes)
+            }
+        } else {
+            ContextCompat.getColor(context, R.color.grey0)
+        }
+    }
+
+    fun setTeamLogoOpacity(imageView: ImageView, isCheerTeam: Boolean) {
+        if (!isCheerTeam) {
+            imageView.alpha = 0.6f
+        } else {
+            imageView.alpha = 1.0f
+        }
+    }
+}
