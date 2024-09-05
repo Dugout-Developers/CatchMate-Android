27d1f8757e5b051fd8397992f4cd521e43478d21
nueijeel <lge6716@gmail.com>
2024-09-06 00:02:42 +0900

    feature: 성별 정보 처리 로직 GenderUtils 생성

---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/util/GenderUtils.kt
@@ -0,0 +1,21 @@
+package com.catchmate.presentation.util
+
+import android.content.Context
+import com.catchmate.presentation.R
+
+object GenderUtils {
+    fun convertPostGender(gender: String): String? {
+        return when (gender) {
+            "여성" -> "F"
+            "남성" -> "M"
+            else -> null
+        }
+    }
+
+    fun convertBoardGender(context: Context, gender: String): String {
+        return when(gender) {
+            "F" -> context.getString(R.string.female)
+            else -> context.getString(R.string.male)
+        }
+    }
+}
