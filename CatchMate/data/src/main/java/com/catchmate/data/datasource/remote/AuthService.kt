1d58b2e7c4267c761644ff4d8726569d77e253ec
nueijeel <lge6716@gmail.com>
2024-10-01 23:14:59 +0900

    feature: 토큰 재발급을 위한 reissue api 구현

--- CatchMate/data/src/main/java/com/catchmate/data/datasource/remote/AuthService.kt
+++ CatchMate/data/src/main/java/com/catchmate/data/datasource/remote/AuthService.kt
@@ -2,8 +2,10 @@ package com.catchmate.data.datasource.remote
 
 import com.catchmate.data.dto.LoginRequestDTO
 import com.catchmate.data.dto.LoginResponseDTO
+import com.catchmate.data.dto.ReissueResponseDTO
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface AuthService {
@@ -11,4 +13,9 @@ interface AuthService {
     suspend fun postLogin(
         @Body loginRequestDTO: LoginRequestDTO,
     ): Response<LoginResponseDTO?>
+
+    @POST("auth/reissue")
+    suspend fun postReissue(
+        @Header("RefreshToken") refreshToken: String,
+    ): Response<ReissueResponseDTO?>
 }
---
+++ CatchMate/data/src/main/java/com/catchmate/data/dto/ReissueResponseDTO.kt
@@ -0,0 +1,5 @@
+package com.catchmate.data.dto
+
+data class ReissueResponseDTO(
+    val accessToken: String,
+)
