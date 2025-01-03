b8f919a0544c485535446c9091f738d2d162d609
nueijeel <lge6716@gmail.com>
2025-01-03 10:18:55 +0900

    feature: notification fragment에 viewmodel 값 존재 여부에 따른 분기 구현

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/notification/NotificationFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/notification/NotificationFragment.kt
@@ -5,13 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import com.catchmate.presentation.R
 import com.catchmate.presentation.databinding.FragmentNotificationBinding
+import com.catchmate.presentation.viewmodel.NotificationViewModel
 
 class NotificationFragment : Fragment() {
     private var _binding: FragmentNotificationBinding? = null
     val binding get() = _binding!!
+
+    private val notificationViewModel: NotificationViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,6 +34,7 @@ class NotificationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initHeader()
+        initViewModel()
     }
 
     override fun onDestroyView() {
@@ -43,5 +49,22 @@ class NotificationFragment : Fragment() {
                 findNavController().popBackStack()
             }
         }
+    }
+
+    private fun initViewModel() {
+        notificationViewModel.getReceivedNotificationList()
+        notificationViewModel.receivedNotificationList.observe(viewLifecycleOwner) { response ->
+            if (response.content.isEmpty()) {
+                binding.rvNotificationList.visibility = View.GONE
+                binding.layoutNotificationNoList.visibility = View.VISIBLE
+            } else {
+                binding.rvNotificationList.visibility = View.VISIBLE
+                binding.layoutNotificationNoList.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun initRecyclerView() {
+
     }
 }
