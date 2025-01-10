e6321ac10a3ffd9027fd8181beef1f15feeec226
nueijeel <lge6716@gmail.com>
2025-01-10 16:51:25 +0900

    ui: home filter view 배경 리소스 수정

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFilterView.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFilterView.kt
@@ -6,7 +6,6 @@ import android.content.res.TypedArray
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.ViewCompat
 import com.catchmate.presentation.R
 import com.catchmate.presentation.databinding.ViewHomeFilterBinding
 
@@ -32,11 +31,9 @@ class HomeFilterView(
     }
 
     private fun setTypedArray(typedArray: TypedArray) {
-        val homeFilterLayoutColor = typedArray.getColor(R.styleable.HomeFilterView_homeFilterLayoutColor, -1)
         val homeFilterNameText = typedArray.getText(R.styleable.HomeFilterView_homeFilterNameText)
         val homeFilterImage = typedArray.getResourceId(R.styleable.HomeFilterView_homeFilterImage, -1)
 
-        ViewCompat.setBackgroundTintList(binding.layoutFilter, ColorStateList.valueOf(homeFilterLayoutColor))
         binding.tvFilterName.text = homeFilterNameText
         binding.ivFilterDropdown.setImageResource(homeFilterImage)
     }
