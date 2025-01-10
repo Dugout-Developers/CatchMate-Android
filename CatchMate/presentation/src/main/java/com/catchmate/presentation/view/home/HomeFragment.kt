bd4e8e0c163dbb0537274045e96537bc366472dd
nueijeel <lge6716@gmail.com>
2025-01-10 17:58:07 +0900

    refactor: 미사용 improt 제거

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/home/HomeFragment.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
