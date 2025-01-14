39cfc7560fedf3212c262ca0c38fbd5e2989597e
nueijeel <lge6716@gmail.com>
2025-01-15 00:48:20 +0900

    fix: UserService api endpoint 수정

--- CatchMate/data/src/main/java/com/catchmate/data/datasource/remote/UserService.kt
+++ CatchMate/data/src/main/java/com/catchmate/data/datasource/remote/UserService.kt
@@ -17,26 +17,26 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UserService {
-    @GET("user/profile")
+    @GET("users/profile")
     suspend fun getUserProfile(): Response<GetUserProfileResponseDTO?>
 
-    @GET("user/profile/{profileUserId}")
+    @GET("users/profile/{profileUserId}")
     suspend fun getUserProfileById(
         @Path("profileUserId") profileUserId: Long,
     ): Response<GetUserProfileByIdResponseDTO?>
 
-    @POST("user/additional-info")
+    @POST("users/additional-info")
     suspend fun postUserAdditionalInfo(
         @Body postUserAdditionalInfoRequestDTO: PostUserAdditionalInfoRequestDTO,
     ): Response<PostUserAdditionalInfoResponseDTO?>
 
-    @PATCH("user/profile")
+    @PATCH("users/profile")
     suspend fun patchUserProfile(
         @Body patchUserProfileRequestDTO: PatchUserProfileRequestDTO,
     ): Response<PatchUserProfileResponseDTO?>
 
     // isEnabled : Y/N
-    @PATCH("user/alarm")
+    @PATCH("users/alarm")
     suspend fun patchUserAlarm(
         @Query("alarmType") alarmType: AlarmType,
         @Query("isEnabled") isEnabled: String,
