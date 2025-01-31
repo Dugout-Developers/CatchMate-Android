b82b37fdf9439a864e2ce568e1ab37bd1caa7848
nueijeel <lge6716@gmail.com>
2025-02-01 01:10:47 +0900

    feature: 보낸 신청 목록 불러오는 로직 구현

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/SentJoinFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/mypage/SentJoinFragment.kt
@@ -5,13 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.catchmate.presentation.R
 import com.catchmate.presentation.databinding.FragmentSentJoinBinding
+import com.catchmate.presentation.interaction.OnPostItemClickListener
+import com.catchmate.presentation.viewmodel.SentJoinViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-class SentJoinFragment : Fragment() {
+@AndroidEntryPoint
+class SentJoinFragment :
+    Fragment(),
+    OnPostItemClickListener {
     private var _binding: FragmentSentJoinBinding? = null
     val binding get() = _binding!!
+
+    private val sentJoinViewModel: SentJoinViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +38,8 @@ class SentJoinFragment : Fragment() {
     ) {
         super.onViewCreated(view, savedInstanceState)
         initHeader()
+        initRecyclerView()
+        initViewModel()
     }
 
     override fun onDestroyView() {
@@ -42,5 +54,28 @@ class SentJoinFragment : Fragment() {
             }
             tvHeaderTextTitle.text = getString(R.string.mypage_sent_join)
         }
+    }
+
+    private fun initRecyclerView() {
+        binding.rvSentJoinPostList.apply {
+            adapter = SentJoinAdapter(requireContext(), layoutInflater, this@SentJoinFragment)
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+    }
+
+    private fun initViewModel() {
+        sentJoinViewModel.getRequestedEnrollList()
+        sentJoinViewModel.requestedEnrollList.observe(viewLifecycleOwner) { response ->
+            if (response != null) {
+                val adapter = binding.rvSentJoinPostList.adapter as SentJoinAdapter
+                adapter.updateList(response.enrollInfoList)
+            }
+        }
+    }
+
+    override fun onPostItemClicked(boardId: Long) {
+        val bundle = Bundle()
+        bundle.putLong("boardId", boardId)
+        findNavController().navigate(R.id.action_sentJoinFragment_to_readPostFragment, bundle)
     }
 }
