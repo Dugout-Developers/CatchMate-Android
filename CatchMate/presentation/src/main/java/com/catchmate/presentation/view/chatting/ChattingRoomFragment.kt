4c5ae70aaa7f4fea72f964edd99b4d56abe8119b
nueijeel <lge6716@gmail.com>
2025-02-24 22:05:48 +0900

    refactor: remove unused import

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/chatting/ChattingRoomFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/chatting/ChattingRoomFragment.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.View
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.viewModels
 import androidx.navigation.NavOptions
