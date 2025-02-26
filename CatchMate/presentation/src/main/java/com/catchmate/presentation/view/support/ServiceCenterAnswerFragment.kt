f9a24d1d17765acaff7034c541a7e358e9c125a8
nueijeel <lge6716@gmail.com>
2025-02-26 11:09:44 +0900

    feature: 문의 답변 데이터 불러와 화면에 표시

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/support/ServiceCenterAnswerFragment.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/support/ServiceCenterAnswerFragment.kt
@@ -2,17 +2,27 @@ package com.catchmate.presentation.view.support
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.catchmate.presentation.R
 import com.catchmate.presentation.databinding.FragmentServiceCenterAnswerBinding
+import com.catchmate.presentation.util.DateUtils.formatInquiryAnsweredDate
 import com.catchmate.presentation.view.base.BaseFragment
+import com.catchmate.presentation.viewmodel.ServiceCenterAnswerViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ServiceCenterAnswerFragment : BaseFragment<FragmentServiceCenterAnswerBinding>(FragmentServiceCenterAnswerBinding::inflate) {
+    private val serviceCenterAnswerViewModel: ServiceCenterAnswerViewModel by viewModels()
+    private val inquiryId by lazy { arguments?.getLong("inquiryId") ?: -1L }
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+        initViewModel()
+        serviceCenterAnswerViewModel.getInquiry(inquiryId)
         initView()
     }
 
@@ -22,12 +32,20 @@ class ServiceCenterAnswerFragment : BaseFragment<FragmentServiceCenterAnswerBind
             layoutHeaderServiceCenterAnswer.imgbtnHeaderTextBack.setOnClickListener {
                 findNavController().popBackStack()
             }
+        }
+    }
 
-            val title = getString(R.string.service_center_answer_title)
-            tvTitleServiceCenterAnswer.text = title.format("nickname")
-//            tvDateServiceCenterAnswer.text =
-//            tvInquiryContentServiceCenterAnswer.text =
-//            tvAnswerContentServiceCenterAnswer.text =
+    private fun initViewModel() {
+        serviceCenterAnswerViewModel.getInquiryResponse.observe(viewLifecycleOwner) { response ->
+            response?.let {
+                binding.apply {
+                    val title = getString(R.string.service_center_answer_title)
+                    tvTitleServiceCenterAnswer.text = title.format(response.nickName)
+                    tvDateServiceCenterAnswer.text = formatInquiryAnsweredDate(response.createdAt)
+                    tvInquiryContentServiceCenterAnswer.text = response.content
+                    tvAnswerContentServiceCenterAnswer.text = response.answer
+                }
+            }
         }
     }
 }
