00760bffd497800de82c12ba31481aaeb8dae73a
nueijeel <lge6716@gmail.com>
2026-02-15 23:27:39 +0900

    ui: TermsAndCondition 세부 및 전체 Composable 구현

--- CatchMate/presentation/src/main/java/com/catchmate/presentation/view/components/Buttons.kt
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/components/Buttons.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -88,6 +89,26 @@ fun CatchMateBackButton(
         Icon(
             painter = painterResource(R.drawable.vec_all_left_arrow_20dp),
             contentDescription = null,
+        )
+    }
+}
+
+@Composable
+fun CatchMateIconButton(
+    iconRes: Int,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Unspecified,
+) {
+    IconButton(
+        onClick = onClick,
+        modifier =
+            modifier
+                .size(24.dp),
+    ) {
+        CatchMateIcon(
+            iconRes = iconRes,
+            tint = tint,
         )
     }
 }
---
+++ CatchMate/presentation/src/main/java/com/catchmate/presentation/view/onboarding/TermsAndConditionScreen.kt
@@ -0,0 +1,143 @@
+package com.catchmate.presentation.view.onboarding
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.catchmate.presentation.R
+import com.catchmate.presentation.view.components.ButtonType
+import com.catchmate.presentation.view.components.CatchMateFilledButton
+import com.catchmate.presentation.view.components.CatchMateIcon
+import com.catchmate.presentation.view.components.CatchMateIndicator
+import com.catchmate.presentation.view.components.CatchMateTopAppBar
+import com.catchmate.presentation.view.theme.CatchMateTextStyle.Body02Medium
+import com.catchmate.presentation.view.theme.CatchMateTextStyle.HeadLine01Regular
+import com.catchmate.presentation.view.theme.Grey0
+import com.catchmate.presentation.view.theme.Grey500
+import com.catchmate.presentation.view.theme.Grey800
+
+@Composable
+fun TermsAndConditionScreen(
+    onBackClick: () -> Unit,
+    onNextClick: () -> Unit,
+    onAllAgreementRowCheckedChange: (Boolean) -> Unit,
+    onServiceTermsRowCheckedChange: (Boolean) -> Unit,
+    onPrivacyPolicyRowCheckedChange: (Boolean) -> Unit,
+    onMarketingPushRowCheckedChange: (Boolean) -> Unit,
+    onServiceTermsDetailClick: () -> Unit,
+    onPrivacyPolicyDetailClick: () -> Unit,
+    onMarketingPushDetailClick: () -> Unit,
+    isBottomButtonEnable: Boolean,
+    isAllAgreementChecked: Boolean,
+    isServiceTermsChecked: Boolean,
+    isPrivacyPolicyChecked: Boolean,
+    isMarketingPushChecked: Boolean,
+) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        containerColor = Grey0,
+        topBar = {
+            CatchMateTopAppBar(
+                onBackClick = onBackClick,
+                actions = {
+                    CatchMateIndicator(currentPage = 1)
+                }
+            )
+        },
+        bottomBar = {
+            Column(
+                modifier =
+                    Modifier
+                        .fillMaxWidth(),
+            ) {
+                CatchMateFilledButton(
+                    text = stringResource(R.string.next),
+                    onClick = onNextClick,
+                    enabled = isBottomButtonEnable,
+                    buttonType = ButtonType.FILLED,
+                    modifier =
+                        Modifier
+                            .padding(horizontal = 12.dp, vertical = 4.dp),
+                )
+                Spacer(Modifier.height(34.dp))
+            }
+
+        },
+    ) { innerPadding ->
+        Column(
+            modifier =
+                Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = 24.dp)
+                    .padding(top = 52.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.tac_title_1),
+                style = HeadLine01Regular,
+                color = Grey800,
+            )
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = stringResource(R.string.tac_title_2),
+                    style = HeadLine01Regular,
+                    color = Grey800,
+                )
+                Spacer(modifier = Modifier.width(6.dp))
+                CatchMateIcon(
+                    iconRes = R.drawable.vec_all_essential_mark_6dp,
+                )
+            }
+            Spacer(Modifier.height(39.dp))
+            TermsAndConditionAllAgreementRow(
+                isChecked = isAllAgreementChecked,
+                onCheckedChange = { onAllAgreementRowCheckedChange(!isAllAgreementChecked) },
+            )
+            Spacer(Modifier.height(20.dp))
+            Text(
+                text = stringResource(R.string.tac_terms_title),
+                style = Body02Medium,
+                color = Grey500,
+            )
+            Spacer(Modifier.height(10.dp))
+            TermsAndConditionCheckRow(
+                text = stringResource(R.string.tac_content_1),
+                isChecked = isServiceTermsChecked,
+                onCheckedChange = { onServiceTermsRowCheckedChange(!isServiceTermsChecked) },
+                onDetailClick = onServiceTermsDetailClick,
+            )
+            TermsAndConditionCheckRow(
+                text = stringResource(R.string.tac_content_2),
+                isChecked = isPrivacyPolicyChecked,
+                onCheckedChange = { onPrivacyPolicyRowCheckedChange(!isPrivacyPolicyChecked) },
+                onDetailClick = onPrivacyPolicyDetailClick,
+            )
+            TermsAndConditionCheckRow(
+                text = stringResource(R.string.tac_content_3),
+                isChecked = isMarketingPushChecked,
+                onCheckedChange = { onMarketingPushRowCheckedChange(!isMarketingPushChecked) },
+                onDetailClick = onMarketingPushDetailClick,
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewTermsAndConditionScreen() {
+    TermsAndConditionScreen({}, {}, {}, {}, {}, {}, {}, {}, {}, true, false, false, false, false)
+}
